27f6abe7fae962c8f2ff5c5dfc76eb84fc9f9ee6
benjaminha14 <benjaminha14@gmail.com>
2020-11-29 19:07:28 -0800

    5 minute sunset timer plus lofi background music added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { Vector3, TextureLoader,RepeatWrapping } from "three";
 import {useLoader} from "react-three-fiber"
-import { StandardEnvironment, Background, Logo } from "spacesvr";
+import { StandardEnvironment, Background, Logo, Audio } from "spacesvr";
 import Floor from "./components/Floor"
 import {Sky} from "@react-three/drei"
 import RealIsland from "./models/RealIsland"
+import TimerSky from "./components/TimerSky"
 
 
 function App() {
@@ -14,18 +15,9 @@ function App() {
       <StandardEnvironment player={{ pos: new Vector3(5, 1, 0), rot: Math.PI}}>
 
         <ambientLight />
-        <Sky
-          distance = {200}
-          inclination={0.5} // Sun elevation angle from 0 to 1 (default=0)
-          azimuth={0.25} // Sun rotation around the Y axis from 0 to 1 (default=0.25)
-          turbidity = {10}
-          rayleigh = {2.376}
-          mieCoefficient = {0.005}
-          mieDirectionalIG = {0.7}
-          exposure = {0.5}
-
-        />
+        <TimerSky />
         <RealIsland/>
+        <Audio url="lofi.mp3" position={new Vector3(0, 4, 0)} />
 
         <Floor/>
       </StandardEnvironment>
